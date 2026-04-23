a8f0eb2dba4d7c7498fe8ab7c84936e61089e887
felipetojal <felipetojal07@gmail.com>
2026-04-23 14:23:20 -0300

    changing the bitmap representation: now, each byte will use its full 8 bit capacity, improving the memory usage and efficiency

--- internal/volume/bitmap.go
+++ internal/volume/bitmap.go
@@ -10,19 +10,44 @@ const (
 // It is stored in the volume.dat file as a header
 // (bytes 0 - 4095).
 type BitMap struct {
-	// 1 - Occupied
-	// 0 - Free
+	// Each byte contains 8 bits. Since we are only
+	// storing 0s and 1s, we will split the bytes
+	// to avoid memory waste.
 	bitMap []byte
 }
 
-// occupyPosition occupies the position at the given index.
-func (b *BitMap) occupyPosition(position int) {
-	b.bitMap[position] = 1
+// freePosition frees the position at the given index.
+// position is the block index in the volume.
+func (b *BitMap) freePosition(position int) {
+	/*
+	* Example: Say the position (block index) is 45.
+	* Since we are operating the bits of the bytes, and
+	* each byte contains 8 bits, we make a mod operation
+	* in the position to extract the right bitmap index.
+	*
+	* 43 / 8 = 5 -> bitmap index is 5
+	* 43 % 8 = 3 -> location within the bitmap byte.
+	 */
+
+	// Getting the right indexes.
+	bitMapIndex := position / 8
+	byteIndex := position % 8
+
+	// Setting the byteIndex to 0, indicating that it is
+	// now free.
+	b.bitMap[bitMapIndex] = b.bitMap[bitMapIndex] &^ (1 << byteIndex)
 }
 
-// freePosition frees the position at the given index.
-func (b *BitMap) freePosition(position int) {
-	b.bitMap[position] = 0
+// occupyPosition occupies the position at the given index.
+// position is the block index in the volume.
+func (b *BitMap) occupyPosition(position int) {
+	// Getting the right indexes.
+	bitMapIndex := position / 8
+	byteIndex := position % 8
+
+	// Setting the byteIndex to 1, indicating that it is
+	// now occupied.
+	b.bitMap[bitMapIndex] = b.bitMap[bitMapIndex] | (1 << byteIndex)
 }
 
 // loadBitMap loads the bit map from the volume file.
