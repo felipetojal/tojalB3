958bf55b3d5ad25e40f8a9ff7d383dc8ad350a91
felipetojal <felipetojal07@gmail.com>
2026-04-26 10:21:26 -0300

    adding IndexTable methods and tests

--- internal/metadata/index.go
+++ internal/metadata/index.go
@@ -1,5 +1,7 @@
 package metadata
 
+import "fmt"
+
 // Index is a struct that holds data
 // of a certain block.
 type Index struct {
@@ -18,6 +20,15 @@ type Index struct {
 	refCount int
 }
 
+// newIndex creates a new Index.
+func newIndex(hash string, address int) *Index {
+	return &Index{
+		hash:     hash,
+		address:  address,
+		refCount: 1,
+	}
+}
+
 // IndexTable is a struct that maps block hashes
 // to Index objects. This will allow O(1) lookups.
 type IndexTable struct {
@@ -25,16 +36,48 @@ type IndexTable struct {
 }
 
 // newIndexTable creates a new IndexTable.
-func newIndexTable() (*IndexTable) {
+func newIndexTable() *IndexTable {
 	return &IndexTable{
 		indexes: make(map[string]Index),
 	}
 }
 
-// newIndex creates a new Index.
-func newIndex(hash string, address int) *Index {
-	return &Index{
-		hash:    hash,
-		address: address,
+// getIndex receives a block hash and returns the
+// index with the block information.
+func (it *IndexTable) getIndex(hash string) (*Index, error) {
+	if !checkExistence(it, hash) {
+		return nil, fmt.Errorf("error: hash does not exist in IndexTable.")
 	}
+
+	index := it.indexes[hash]
+
+	return &index, nil
+}
+
+// addIndex receives an Index and adds it to the IndexTable.
+// If the index does not yet exists, we must cerate a new
+// key-value pair. If it already exists and we are trying to
+// add it again, we must increase the refCount.
+func (it *IndexTable) addIndex(i Index) {
+	// Getting the original block hash.
+	hash := i.hash
+	// Declaring an Index variable in the function scope with
+	// the same value as i.
+	index := i
+	// Check the existence of the given hash.
+	if checkExistence(it, hash) {
+		// If it the hash does exist, we update the refCount.
+		index = it.indexes[hash]
+		index.refCount = index.refCount + 1
+	}
+
+	// Storing the Index in the map
+	it.indexes[hash] = index
+}
+
+// checkExistence is an auxiliary function used to check
+// if a block hash already exists in the IndexTable.
+func checkExistence(it *IndexTable, hash string) bool {
+	_, ok := it.indexes[hash]
+	return ok
 }
--- internal/metadata/index_test.go
+++ internal/metadata/index_test.go
@@ -15,6 +15,7 @@ func TestNewIndex(t *testing.T) {
 	address := 34
 	i := newIndex(hash, address)
 	a.NotNil(i)
+	a.Equal(1, i.refCount)
 }
 
 // TestNewTable is responsible for testing
@@ -24,4 +25,63 @@ func TestNewIndexTable(t *testing.T) {
 
 	it := newIndexTable()
 	a.NotNil(it)
+}
+
+// TestGetIndex is responsible for testing
+// the getIndex function associated to IndexTable.
+func TestGetIndex(t *testing.T) {
+	a := assert.New(t)
+
+	// Creating the Index object.
+	hash := "vnwerogvweg23ujg234"
+	address := 45
+	i := newIndex(hash, address)
+	a.NotNil(i)
+
+	// Creating the table and adding the Index.
+	it := newIndexTable()
+	a.NotNil(it)
+	it.addIndex(*i)
+
+	// Getting the Index object.
+	i, err := it.getIndex(hash)
+	a.NotNil(i)
+	a.Nil(err)
+}
+
+// TestAddIndex is responsible for testing
+// the addIndex function associated to IndexTable.
+func TestAddIndex(t *testing.T) {
+	a := assert.New(t)
+
+	// Creating the Index object.
+	hash := "vnwerogvweg23ujg234"
+	address := 45
+	i := newIndex(hash, address)
+	a.NotNil(i)
+	a.Equal(1, i.refCount)
+
+	// Creating the table and adding the Index.
+	it := newIndexTable()
+	a.NotNil(it)
+	it.addIndex(*i)
+	savedIndex1 := it.indexes[i.hash]
+	a.Equal(1, savedIndex1.refCount)
+
+	// Checking if the hash we just inserted
+	// is really there.
+	b := checkExistence(it, hash)
+	a.True(b)
+
+	// Creating a new index with the same hash.
+	address = 9
+	i2 := newIndex(hash, address)
+	a.NotNil(i2)
+	it.addIndex(*i2)
+	savedIndex2 := it.indexes[i2.hash]
+	a.Equal(2, savedIndex2.refCount)
+}
+
+func TestCheckExistence(t *testing.T) {
+
 }
