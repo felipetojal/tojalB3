b2154e2da341457e795dc40228f07883b384c2e0
felipetojal <felipetojal07@gmail.com>
2026-04-27 14:31:01 -0300

    added the function to store objects in the database.

--- internal/metadata/database.go
+++ internal/metadata/database.go
@@ -1,7 +1,10 @@
 package metadata
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"reflect"
 
 	badger "github.com/dgraph-io/badger/v4"
 )
@@ -25,6 +28,39 @@ func NewDatabase(dirPath string) (*Database, error) {
 	}, nil
 }
 
+// storeObject will be used to store an object to the database.
+func (d *Database) storeObject(key string, value interface{}) error {
+	var prefix string
 
+	// Getting the object type.
+	t := reflect.TypeOf(value)
+	switch t.Name() {
+	case "Manifest":
+		prefix = "mani:"
+	case "IndexTable":
+		prefix = "it:"
+	}
 
+	// Starting the transaction.
+	txn := d.db.NewTransaction(true)
+	defer txn.Discard()
 
+	// Marshalling the value.
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(value); err != nil {
+		return fmt.Errorf("error marshalling data: %w", err)
+	}
+
+	// Making the transaction
+	dbKey := prefix + key
+	if err := txn.Set([]byte(dbKey), buf.Bytes()); err != nil {
+		return fmt.Errorf("error making transaction: %w", err)
+	}
+
+	// Commiting the transaction.
+	if err := txn.Commit(); err != nil {
+		return fmt.Errorf("error commiting transaction: %w", err)
+	}
+
+	return nil
+}
--- internal/metadata/manifest.go
+++ internal/metadata/manifest.go
@@ -6,36 +6,36 @@ import "time"
 // file metadata.
 type Manifest struct {
 	// File basic information.
-	fileName string
-	fileDir  string
-	fileSize int64
+	FileName string `json:"fileName"`
+	FileDir  string `json:"fileDir"`
+	FileSize int64  `json:"fileSize"`
 
 	// All the block hashes associated to the file.
-	blocks []string
+	Blocks []string `json:"blocks"`
 
 	// Timestamp of creation
-	storedAt time.Time
+	StoredAt time.Time `json:"storedAt"`
 }
 
 // newManifest creates a new Manifest.
 func newManifest(fileName, fileDir string, fileSize int64, blocks []string) *Manifest {
 	return &Manifest{
-		fileName: fileName,
-		fileDir:  fileDir,
-		fileSize: fileSize,
-		blocks:   blocks,
-		storedAt: time.Now().UTC(),
+		FileName: fileName,
+		FileDir:  fileDir,
+		FileSize: fileSize,
+		Blocks:   blocks,
+		StoredAt: time.Now().UTC(),
 	}
 }
 
-// ManifestTable is a struct that holds all 
+// ManifestTable is a struct that holds all
 // the Manifests saved in the database.
 type ManifestTable struct {
 	manifests map[string]Manifest
 }
 
 // newManifestTable creates a new ManifestTable.
-func newManifestTable() (*ManifestTable) {
+func newManifestTable() *ManifestTable {
 	return &ManifestTable{
 		manifests: make(map[string]Manifest),
 	}
