f75649cc637957fd2620c2e17751b7c477969c3d
felipetojal <felipetojal07@gmail.com>
2026-05-11 14:21:37 -0300

    added store command to cobra CLI.

--- cmd/commands.go
+++ cmd/commands.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/felipetojal/tojalB3/internal/engine"
 	"github.com/felipetojal/tojalB3/internal/metadata"
@@ -24,22 +25,45 @@ var (
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			db, err := metadata.NewDatabase(dbDirPath)
 			if err != nil {
+				log.Printf("Error creating engine: %v", err)
 				return fmt.Errorf("failed to create database: %w", err)
 			}
+			log.Println("Database successfully initialized!")
 
 			v := volume.NewVolumeManager(volumePath)
+			log.Println("Volume successfully loaded!")
 
 			it, err := db.LoadIndexTable()
 			if err != nil {
+				log.Printf("Error creating engine: %v", err)
 				return fmt.Errorf("failed to load index table: %w", err)
 			}
+			log.Println("Index Table loaded!")
 
 			eng, err = engine.NewEngine(v, db, it)
 			if err != nil {
+				log.Printf("Error creating engine: %v", err)
 				return fmt.Errorf("failed to create engine: %w", err)
 			}
+			log.Println("Engine created!")
 
 			return nil
+		},
+	}
+
+	storeCmd = &cobra.Command{
+		Use:   "Store command",
+		Short: "This command is used to store a file in the block storage engine.",
+		Run: func(cmd *cobra.Command, args []string) {
+			file := args[0]
+			log.Println("Store command was called...")
+			log.Printf("File: %v\n", file)
+
+			if err := eng.StoreFile(file); err != nil {
+				log.Fatal("Error: Unable to store file: %w", err)
+			}
+
+			log.Println("File was stored successfully!")
 		},
 	}
 )
