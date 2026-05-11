d4e5b3ec2684190919aaab1268cc03b547c3787c
felipetojal <felipetojal07@gmail.com>
2026-05-11 20:45:28 -0300

    added tests to volume manager.

--- internal/volume/volume_test.go
+++ internal/volume/volume_test.go
@@ -1,6 +1,7 @@
 package volume
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -30,4 +31,50 @@ func TestLoadBitMap(t *testing.T) {
 	bitMap, err := loadBitMap(f)
 	assert.Nil(err)
 	assert.NotNil(bitMap)
+}
+
+func TestStoreBlock(t *testing.T) {
+	block := bytes.Repeat([]byte("25954gn"), block_size)
+
+	assert := assert.New(t)
+
+	tempDir := t.TempDir()
+	vm := NewVolumeManager(tempDir + "volume_test.dat")
+	assert.NotNil(vm)
+
+	_, err := vm.StoreBlock(block)
+	assert.Nil(err)
+}
+
+func TestGetBlock(t *testing.T) {
+	block := bytes.Repeat([]byte("1"), block_size)
+
+	assert := assert.New(t)
+
+	tempDir := t.TempDir()
+	vm := NewVolumeManager(tempDir + "volume_test.dat")
+	assert.NotNil(vm)
+
+	pos, err := vm.StoreBlock(block)
+	assert.Nil(err)
+
+	b, err := vm.GetBlock(pos)
+	assert.Nil(err)
+	assert.Equal(b, block)
+}
+
+func TestVolumeDeleteBlock(t *testing.T) {
+	block := bytes.Repeat([]byte("1"), block_size)
+
+	assert := assert.New(t)
+
+	tempDir := t.TempDir()
+	vm := NewVolumeManager(tempDir + "volume_test.dat")
+	assert.NotNil(vm)
+
+	pos, err := vm.StoreBlock(block)
+	assert.Nil(err)
+
+	err = vm.DeleteBlock(pos)
+	assert.Nil(err)
 }
