5ab8c6d87f5474747ded11ba99f3884518359789
felipetojal <felipetojal07@gmail.com>
2026-05-11 19:40:05 -0300

    list command.

--- cmd/commands.go
+++ cmd/commands.go
@@ -53,6 +53,10 @@ var (
 		Use:   "store",
 		Short: "This command is used to store a file in the block storage engine.",
 		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) != 1 {
+				fmt.Println("Usage: store <file>")
+				return
+			}
 			file := args[0]
 			fmt.Println("Store command was called...")
 			fmt.Printf("File: %v\n", file)
@@ -62,6 +66,25 @@ var (
 			}
 
 			fmt.Println("File was stored successfully!")
+		},
+	}
+
+	deleteCmd = &cobra.Command{
+		Use:   "delete",
+		Short: "This command is used to delete a file from the block storage engine.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) != 1 {
+				fmt.Println("Usage: delete <file>")
+				return
+			}
+			file := args[0]
+			fmt.Println("Delete command was called.")
+
+			if err := eng.DeleteFile(file); err != nil {
+				fmt.Println("Error: Unable to delete file: %w", err)
+				return
+			}
+			fmt.Println("File was deleted successfully!")
 		},
 	}
 
@@ -84,11 +107,32 @@ var (
 			fmt.Println("File was retrieved successfully!")
 		},
 	}
+
+	listCmd = &cobra.Command{
+		Use:   "list",
+		Short: "This command is used to list all the files stored in the block engine.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("List command was called.")
+
+			files := eng.ListFiles()
+
+			if len(files) == 0 {
+				fmt.Println("No files found.")
+				return
+			}
+
+			for _, file := range files {
+				fmt.Println(file)
+			}
+		},
+	}
 )
 
 func init() {
 	rootCmd.AddCommand(storeCmd)
 	rootCmd.AddCommand(getCmd)
+	rootCmd.AddCommand(deleteCmd)
+	rootCmd.AddCommand(listCmd)
 }
 
 func Execute() error {
