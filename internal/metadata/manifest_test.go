5fffde54c19e6dbca869195904c9dd0b925d3bbf
felipetojal <felipetojal07@gmail.com>
2026-05-12 08:30:00 -0300

    added test to manifest.

--- internal/metadata/manifest_test.go
+++ internal/metadata/manifest_test.go
@@ -1,6 +1,8 @@
 package metadata
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,4 +19,25 @@ func TestNewManifest(t *testing.T) {
 	m := NewManifest(fileName, fileSize)
 	m.AddBlock(blocks...)
 	a.NotNil(m)
+}
+
+func TestAddBlock(t *testing.T) {
+	tempDir := t.TempDir()
+
+	fileName := "jojo.txt"
+	filePath := filepath.Join(tempDir, fileName)
+	f, err := os.Create(filePath)
+	assert.NoError(t, err)
+	assert.NotNil(t, f)
+	defer f.Close()
+
+	m := NewManifest(fileName, 7000)
+	assert.NotNil(t, m)
+
+	blocks := []string{"ogwrio", "novn249gn249"}
+
+	m.AddBlock(blocks...)
+
+	l := len(m.Blocks)
+	assert.Equal(t, 2, l)
 }
